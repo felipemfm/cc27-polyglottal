35ac3c2aa72562b310fccf394a05a155d95b293a
felipemfm <felipemfm@gmail.com>
2022-08-29 15:10:39 +0900

    Feat: Add GET /railway/{line_or_station} endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,3 +15,8 @@ async def root():
 @app.get("/railway")
 def read_railway_data():
     return api.read_railway_data()
+
+
+@app.get("/railway/{line_or_station}")
+def read_railway_data_by(line_or_station):
+    return api.read_railway_data_by(line_or_station)
