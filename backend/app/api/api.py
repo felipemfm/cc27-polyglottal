a6b47b375fa6b3f21a6fbc610aa3211170efa013
felipemfm <felipemfm@gmail.com>
2022-08-29 15:11:32 +0900

    Feat: Add /railway/{line_or_station} endpoint controller function

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -5,3 +5,14 @@ def read_railway_data():
         data = json.load(stream)
 
     return data
+
+def read_railway_data_by(line_or_station):
+    selected_data = []
+    with open('data/railway.json', 'r',encoding='utf-8', errors='ignore') as stream:
+        data = json.load(stream)
+
+    for point in data:
+        if line_or_station in point['line'].lower() or line_or_station in point['station'].lower():
+            selected_data.append(point)
+    
+    return selected_data
