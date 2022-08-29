be78d1a065beca23a163d9a3bfc3caceb675ddd3
felipemfm <felipemfm@gmail.com>
2022-08-29 21:43:03 +0900

    Feat: Add environment variables support

--- backend/app/api/api.py
+++ backend/app/api/api.py
@@ -1,4 +1,11 @@
 import json
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# print(os.getenv("API_KEY"))
 
 def read_railway_data():
     with open('data/railway.json', 'r',encoding='utf-8', errors='ignore') as stream:
@@ -6,7 +13,7 @@ def read_railway_data():
 
     return data
 
-def read_railway_data_by(line_or_station):
+def read_railway_data_by(line_or_station: str):
     selected_data = []
     with open('data/railway.json', 'r',encoding='utf-8', errors='ignore') as stream:
         data = json.load(stream)
@@ -16,3 +23,5 @@ def read_railway_data_by(line_or_station):
             selected_data.append(point)
     
     return selected_data
+
+    
