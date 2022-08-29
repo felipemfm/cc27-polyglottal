a3d17d8adedda6c61a4af89bfdf7f7764f9b9f9c
felipemfm <felipemfm@gmail.com>
2022-08-29 15:07:45 +0900

    Feat: Get /railway endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,8 @@
+# uvicorn app.main:app --reload
+
 from fastapi import FastAPI
+
+from app.api import api
 
 app = FastAPI()
 
@@ -7,3 +11,7 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+
+@app.get("/railway")
+def read_railway_data():
+    return api.read_railway_data()
